c7a4fba8ae3c55cd8150745ac2b223e0869844d3
bleclub <bleclub_5@live.com>
2017-10-09 02:03:57 +0700

    Update video section / news section / follow us section and footer

--- html/assets/js/script.js
+++ html/assets/js/script.js
@@ -132,6 +132,16 @@ $(document).ready(function () {
         autoplay: false
     });
 
+    $('.video').parent().click(function () {
+        if($(this).children(".video").get(0).paused){
+            $(this).children(".video").get(0).play();
+            $(this).children(".playpause").fadeOut();
+        }else{
+           $(this).children(".video").get(0).pause();
+            $(this).children(".playpause").fadeIn();
+        }
+    });
+
 });
 
 //----- Popup Start ------
